884d08f3cccc5b41dffae2650fa2c12be4318744
r-snell1 <162180188+r-snell1@users.noreply.github.com>
2024-04-08 11:06:37 -0400

    Chapter 4 book projects WIP

---
+++ Chapter4/Chapter4No1.py
@@ -0,0 +1,6 @@
+"""
+Write a script that inputs a line of plaintext and a distance value and outputs an
+encrypted text using a Caesar cipher. The script should work for any printable
+characters.
+"""
+
---
+++ Chapter4/Chapter4No2.py
@@ -0,0 +1,5 @@
+"""
+Write a script that inputs a line of encrypted text and a distance value and outputs
+plaintext using a Caesar cipher. The script should work for any printable characters.
+"""
+
