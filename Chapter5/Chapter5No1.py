fd6ea691243e5599a9dcdf3f825885bb4c9d6545
r-snell1 <162180188+r-snell1@users.noreply.github.com>
2024-04-15 10:34:32 -0400

    All chapter 5 book projects completed

--- Chapter5/Chapter5No1.py
+++ Chapter5/Chapter5No1.py
@@ -7,4 +7,35 @@ should expect a list of numbers as an argument and return a single number. Each
 function should return 0 if the list is empty. Include a main function that tests the
 three statistical functions with a given list.
 """
+import statistics
 
+number_set = []
+
+
+def main():
+
+    textfile1 = input("Please enter the first text file: ")
+    number_set = open(textfile1, "r")
+    print(f'the median of the list of numbers is : {median(number_set)}')
+    print(f'the mode of the list of numbers is : {mode(number_set)}')
+    print(f'the mean of the list of numbers is : {mean(number_set)}')
+
+def median(number_set):
+
+    median_number = statistics.median(number_set)
+
+    return median_number
+
+def mode(number_set):
+
+    mode_number = statistics.mode(number_set)
+
+    return mode_number
+
+def mean(number_set):
+
+    mean_number = statistics.mean(number_set)
+
+    return mean_number
+
+main()
